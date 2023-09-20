74fc15d48be86afad8df2672ed6216c6ba45ef55
Melissa Yvette Cavazos <109395704+melissacallis@users.noreply.github.com>
2023-09-20 21:53:04 +0000

    heroku files added

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -57,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 X_FRAME_OPTIONS = "ALLOW-FROM preview.app.github.dev"
